8ca6bc47d38550517a871aace30c194c113580d6
Awais Shams <82948178+Awais-shams@users.noreply.github.com>
2022-07-27 13:53:10 +0500

    testing the vercel deployment

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Asynchronous and ES6 JavaScript Fundamentals</h1>
+      <h6>Finally i have uploaded my react app on vercel</h6>
       <FetchApi/>
     </div>
   );
